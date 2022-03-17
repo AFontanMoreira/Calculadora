be133d99f96c97b212bab7414c35d3d3707e3701
Alex <afontanmoreira@danielcastelao.org>
2022-03-17 22:41:16 +0100

    finalizada calculadora

--- src/calculadora/Servidor.java
+++ src/calculadora/Servidor.java
@@ -14,58 +14,63 @@ public class Servidor {
 
     public static void main(String[] args) throws IOException {
 
-            System.out.println("Creando socket servidor");
+        System.out.println("Creando socket servidor");
 
-            ServerSocket serverSocket = new ServerSocket();
+        ServerSocket serverSocket = new ServerSocket();
 
-            System.out.println("Realizando el bind");
+        System.out.println("Realizando el bind");
 
 
-            InetSocketAddress addr = new InetSocketAddress("localhost",6000);
-            serverSocket.bind(addr);
-            Socket newSocket = serverSocket.accept();
-            System.out.println("Aceptando conexiones");
-            DataInputStream in = new DataInputStream(newSocket.getInputStream());
-            DataOutputStream ou = new DataOutputStream(newSocket.getOutputStream());
+        InetSocketAddress addr = new InetSocketAddress("localhost", 6000);
+        serverSocket.bind(addr);
+        Socket newSocket = serverSocket.accept();
+        System.out.println("Aceptando conexiones");
+        DataInputStream in = new DataInputStream(newSocket.getInputStream());
+        DataOutputStream ou = new DataOutputStream(newSocket.getOutputStream());
 
-            while (true) {
+        while (true) {
 
-                    System.out.println("Antes de la operacion");
-                    String operacionCliente = in.readUTF();
+            System.out.println("Antes de la operacion");
+            String operacionCliente = in.readUTF();
 
-                    System.out.println("Operación " + operacionCliente);
-                    int resultado;
-
-                    StringTokenizer st = new StringTokenizer(operacionCliente);
-
-                    int operando1 ;
-                    String operador ;
-                    int operando2 ;
-
-                    operando1 = Integer.parseInt(st.nextToken());
-                    operador = st.nextToken();
+            System.out.println("Operación " + operacionCliente);
+            float resultado;
+            float operando1 = 0;
+            String operador = null;
+            float operando2 = 0;
+            String[] partes = operacionCliente.split(" ");
+            for (int i = 0; i < partes.length; i++) {
+                if (i == 0) {
+                    operando1 = Float.parseFloat(partes[i]);
+                } else if (i == 1) {
+                    operador = partes[i];
+                } else {
                     try {
-                            operando2 = Integer.parseInt(st.nextToken());
+                        operando2 = Float.parseFloat(partes[i]);
                     } catch (NoSuchElementException e) {
-                            if (operador.equals("/")) {
-                                    operando2 = 1;
-                            } else {
-                                    operando2 = 0;
-                            }
+                        if (operador.equals("/")) {
+                            operando2 = 1;
+                        } else {
+                            operando2 = 0;
+                        }
                     }
+                }
 
-                    if (operador.equals("+")) {
-                            resultado = operando1+operando2;
-                    } else if (operador.equals("-")) {
-                            resultado = operando1-operando2;
-                    } else if (operador.equals("*")) {
-                            resultado = operando1*operando2;
-                    } else {
-                            resultado = operando1/operando2;
-                    }
-
-
-                    ou.writeUTF(Integer.toString(resultado));
             }
+
+
+            if (operador.equals("+")) {
+                resultado = operando1 + operando2;
+            } else if (operador.equals("-")) {
+                resultado = operando1 - operando2;
+            } else if (operador.equals("*")) {
+                resultado = operando1 * operando2;
+            } else {
+                resultado = operando1 / operando2;
+            }
+
+
+            ou.writeUTF(String.valueOf(resultado));
+        }
     }
 }
